6fe7e4865757e05beac47d6a9787656db96ca004
Mageas <web.mageas@protonmail.com>
2021-03-02 17:35:14 +0100

    Remove root only

--- root/install_root.sh
+++ root/install_root.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Make sure only root can run this script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
 # Set workdir
 cd "$(dirname "$0")"
 
