0db50ae06d924f12f4067dc550e2bb841ac4c214
Mageas <web.mageas@protonmail.com>
2021-03-03 10:53:53 +0100

    Refactor install_root.sh

--- root/install_root.sh
+++ root/install_root.sh
@@ -1,36 +1,87 @@
 #!/bin/bash
 
-# Set workdir
-cd "$(dirname "$0")"
-
-# Argument
+#
+# Variables
+#
+SCRIPT_FOLDER="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 NO_CONFIRM=False
-if [[ $1 == "--noconfirm" ]]; then
-    NO_CONFIRM=True
-fi
 
-# List all files
-files=$(find . -type f)
 
-# String to array
-delimiter="./"
-conCatString=$files$delimiter
-splitMultiChar=()
-while [[ $conCatString ]]; do
-    splitMultiChar+=( "${conCatString%%"$delimiter"*}" )
-    conCatString=${conCatString#*"$delimiter"}
-done
+#
+# Set variables from args
+#
+function set_variables_from_args () {
+    case "$1" in
+        --noconfirm)
+            NO_CONFIRM=True
+        ;;
+        *)
+            
+        ;;
+    esac
+}
 
-# Foreach file
-for file in "${splitMultiChar[@]}"; do
-    file=$(echo $file|tr -d '\n')
-    if [[ "$file" != "" && "$file" != "install_root.sh" ]]; then
-        printf "\n./$file --> /$file\n"
-        if [[ $NO_CONFIRM == True ]]; then
-            sudo cp "./$file" "/$file"
-        else
-            sudo cp -i "./$file" "/$file"
-        fi
-        
+
+#
+# Anti ROOT
+#
+function anti_root () {
+    if [[ $EUID -eq 0 ]]; then
+        local RED='\033[0;31m'; local BOLD='\033[1m'
+        printf "${RED}${BOLD}!! Please do not run this script as root !! \n" 1>&2
+        exit 1
     fi
-done
+}
+
+
+#
+# List files
+#
+function list_files () {
+    cd $SCRIPT_FOLDER
+    echo $(find . -type f)
+}
+
+
+#
+# Copy files
+#
+function copy_files () {
+    cd $SCRIPT_FOLDER
+    delimiter="./"
+    conCatString=$*$delimiter
+    splitMultiChar=()
+    while [[ $conCatString ]]; do
+        splitMultiChar+=( "${conCatString%%"$delimiter"*}" )
+        conCatString=${conCatString#*"$delimiter"}
+    done
+
+    for file in "${splitMultiChar[@]}"; do
+        file=$(echo $file|tr -d '\n')
+        if [[ "$file" != "" && "$file" != "install_root.sh" ]]; then
+            printf "\n./$file --> /$file\n"
+            if [[ $NO_CONFIRM == True ]]; then
+                sudo cp "./$file" "/$file"
+            else
+                sudo cp -i "./$file" "/$file"
+            fi
+        fi
+    done
+}
+
+
+#
+# Main
+#
+function main () {
+    anti_root
+
+    set_variables_from_args "$@"
+
+    files=$(list_files)
+
+    copy_files $files
+}
+
+
+main "$@"
